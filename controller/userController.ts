51b07c74d2e094bf5ff81ed2343f0ff791a124e2
Kien Nguyen <ryan@exgo.vn>
2020-06-12 22:53:16 +0700

    demo router

--- controller/userController.ts
+++ controller/userController.ts
@@ -1,6 +1,17 @@
 import { Context, Status } from "https://deno.land/x/oak/mod.ts";
+import Db from "../db/database.ts";
 
-export const testApiHandler = (context: Context) => {
+const testCollection = Db.collection("test");
+
+export const testApiHandler = async (context: Context) => {
+
+  const insertId = await testCollection.insertOne({
+    username: "user1",
+    password: "pass1",
+  });
+
+  console.log(insertId);
+
   context.response.status = Status.OK;
   context.response.body = "testApiHandler";
 };
